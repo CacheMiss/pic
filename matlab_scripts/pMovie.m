2d27681cce39d51589d4df0b490b893b3ff26b8f
Stephen Sewell <stephen.sewell@gmail.com>
2012-07-07 16:49:00 +0100

    Adder user controlled fps selection for movies

--- matlab_scripts/pMovie.m
+++ matlab_scripts/pMovie.m
@@ -1,5 +1,5 @@
 
-function ret = pmovie()
+function ret = pmovie(frameDivider)
    % get file list
    fileList = ls('phi*');
    tmp = size(fileList);
@@ -24,26 +24,23 @@ function ret = pmovie()
    winsize(1:2) = [0 0];
 
    % Set up movie
-   A=moviein(numFiles*5,fig1,winsize);
+   numFrames = numFiles * frameDivider;
+   A=moviein(numFrames,fig1,winsize);
    fps = 1;
 
    set(fig1,'NextPlot','replacechildren')
 
    for i = 1:numFiles
       phi = loadPhi(fileList(i,:));
-      %surf(xValues, yValues, phi);
-      %A(:,i)=getframe(fig1,winsize);
-      %axis([0, xSize, 0, ySize, minPhi, maxPhi]);
       surf(xValues, yValues, phi);
       axis([0, xSize, 0, ySize, minPhi, maxPhi]);
-      A(:,(i-1)*5+1)=getframe(fig1,winsize);
-      A(:,(i-1)*5+2)=getframe(fig1,winsize);
-      A(:,(i-1)*5+3)=getframe(fig1,winsize);
-      A(:,(i-1)*5+4)=getframe(fig1,winsize);
-      A(:,(i-1)*5+5)=getframe(fig1,winsize);
+      for j = 1:frameDivider
+         A(:,(i-1)*frameDivider+j)=getframe(fig1,winsize);
+      end
    end
    clear phi;
    movie(fig1,A,1,fps,winsize);
+   %movie2avi(A, 'test.avi');
 
    ret = 0;
 end
