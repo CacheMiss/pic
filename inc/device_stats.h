a478f3a478fa883b64e56f3e1fd299864b927e0b
Stephen Sewell <stephen.sewell@gmail.com>
2013-05-18 20:26:01 -0500

    Merging back DeviceStats changes to allow me to show how much memory remains free

--- inc/device_stats.h
+++ inc/device_stats.h
@@ -1,12 +1,18 @@
 #ifndef DEVICE_STATS
 #define DEVICE_STATS
 
+#include <cstddef>
 #include <driver_types.h>
 
 struct DeviceStats : public cudaDeviceProp
 {
 public:
-   static DeviceStats & getRef();
+   static DeviceStats & getRef(int dev=-1);
+   std::size_t getTotalMemBytes() const;
+   std::size_t getFreeMemBytes() const;
+   std::size_t getTotalMemMb() const;
+   std::size_t getFreeMemMb() const;
+   double getPercentFreeMem() const;
 private:
    DeviceStats(){}
    static DeviceStats *m_ref;
--- src/device_stats.cpp
+++ src/device_stats.cpp
@@ -1,40 +1,87 @@
 #include "device_stats.h"
 
 #include <cuda_runtime_api.h>
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "error_check.h"
+
 DeviceStats *DeviceStats::m_ref = NULL;
-DeviceStats & DeviceStats::getRef()
+DeviceStats & DeviceStats::getRef(int dev)
 {
    if(m_ref == NULL)
    {
-      m_ref = new DeviceStats();
-      int deviceCount;
-      cudaGetDeviceCount(&deviceCount);
-      if (deviceCount == 0) {
-         fprintf(stderr, "cutil error: no devices supporting CUDA.\n");
-         exit(EXIT_FAILURE);
-      }
-      int dev = 0;
-      cudaGetDeviceProperties(static_cast<cudaDeviceProp*>(m_ref), dev);
-      //cudaDeviceProp deviceProp;
-      //cudaGetDeviceProperties(&deviceProp, dev);
-      if (m_ref->major < 1) {
-         fprintf(stderr, "cutil error: device does not support CUDA.\n");
-         exit(EXIT_FAILURE);
-      }
-      cudaError_t err = cudaSetDevice(dev);
-      if(err != cudaSuccess)
+      try
       {
-         fprintf(stderr,"ERROR Initializing CUDA Device: %s: %s\n", 
-            cudaGetErrorString(err), m_ref->name);
+         m_ref = new DeviceStats();
+         int deviceCount;
+         cudaGetDeviceCount(&deviceCount);
+         if (dev == -1 && deviceCount == 0) 
+         {
+            fprintf(stderr, "cutil error: no devices supporting CUDA.\n");
+            exit(EXIT_FAILURE);
+         }
+         if(dev == -1)
+         {
+            dev = 0;
+         }
+         checkCuda(cudaGetDeviceProperties(static_cast<cudaDeviceProp*>(m_ref), dev));
+         checkCuda(cudaSetDevice(dev));
+         if (m_ref->major < 1) {
+            fprintf(stderr, "Major cuda veresion supported is %d\n", m_ref->major);
+            fprintf(stderr, "cutil error: device does not support CUDA.\n");
+            exit(EXIT_FAILURE);
+         }
+         fprintf(stdout, "Using device %d: %s\n", dev, m_ref->name);
+      }
+      catch (CudaRuntimeError &e)
+      {
+         fprintf(stderr, "ERROR: Unable to initialize a CUDA capable device!\n");
+         fprintf(stderr, "%s", e.what());
          exit(1);
       }
-      fprintf(stderr, "Using device %d: %s\n", dev, m_ref->name);
-
-      //cudaGetDeviceProperties((cudaDeviceProp*)&m_ref, dev);
    }
 
     return *m_ref;
+}
+
+std::size_t DeviceStats::getTotalMemBytes() const
+{
+   std::size_t totalMem;
+   std::size_t freeMem;
+   checkCuda(cudaMemGetInfo(&freeMem, &totalMem));
+
+   return totalMem;
+}
+
+std::size_t DeviceStats::getFreeMemBytes() const
+{
+   std::size_t totalMem;
+   std::size_t freeMem;
+   checkCuda(cudaMemGetInfo(&freeMem, &totalMem));
+
+   return freeMem;
+}
+
+std::size_t DeviceStats::getTotalMemMb() const
+{
+   return getTotalMemBytes() / 1048576;
+}
+
+std::size_t DeviceStats::getFreeMemMb() const
+{
+   return getFreeMemBytes() / 1048576;
+}
+
+double DeviceStats::getPercentFreeMem() const
+{
+   std::size_t totalMem;
+   std::size_t freeMem;
+   checkCuda(cudaMemGetInfo(&freeMem, &totalMem));
+
+   double percentFree = freeMem;
+   percentFree /= totalMem;
+
+   return percentFree;
 }
