c935ec413b40865f94d941c86476134d02b46fea
Stephen Sewell <stephen.sewell@gmail.com>
2013-06-22 12:30:13 -0500

    Updated plot particle script to work. It was broken.

--- matlab_scripts/plotPart.m
+++ matlab_scripts/plotPart.m
@@ -1,4 +1,4 @@
-function ret = plotPart(fName)
+function ret = plotPart(fName, sliceSize)
 
    f = fopen(fName, 'rb');
 
@@ -8,21 +8,33 @@ function ret = plotPart(fName)
       return;
    end
 
-   partStep = 1000;
    numParticles = fread(f, 1, 'int32');
-   particles = fread(f, [numParticles,5], 'float');
-   sparsePart = zeros(numParticles/partStep, 2);
-   for i = 1:partStep:numParticles
-      sparsePart(int32(i/partStep)+1,1) = particles(i,3);
-      sparsePart(int32(i/partStep)+1,2) = particles(i,4);
-   end
+   particles = fread(f, [6, numParticles], 'float');
+   fclose(f);
+   % Remove velocity to save memory
+   particles(3:5,:) = [];
+   xMax = 2^nextpow2(max(particles(1,:)));
+   yMax = 2^nextpow2(max(particles(2,:)));
+   
+   particles = particles(:, 1:sliceSize:end);
+   hotEnd =  find(particles(3,:), 1, 'last');
+   coldBeg = hotEnd + 1;
+   hotP = particles(1:2, 1:hotEnd);
+   coldP = particles(1:2, coldBeg:end);
+   particles = [];
 
    figure;
-   scatter(sparsePart(:,1), sparsePart(:,2), 0.0);
-   title(fName);
+   scatter(hotP(1,:), hotP(2,:), 0.1) % o means no line between points
+   title('Hot');
    xlabel('x');
-   ylabel('x-vel');
+   ylabel('y');
+   axis([0 xMax 0 yMax]);
 
-   fclose(f);
+   figure;
+   scatter(coldP(1,:), coldP(2,:), 0.1) % o means no line between points
+   title('Cold');
+   xlabel('x');
+   ylabel('y');
+   axis([0 xMax 0 yMax]);
 
 end
