61938294be0dc38fa2b52499df97d2328e803002
Stephen Sewell <stephen.sewell@gmail.com>
2013-08-05 18:16:34 -0500

    I was injecting across too wide a space on the bottom of the grid

--- launch_scripts/curveHigh.bat
+++ launch_scripts/curveHigh.bat
@@ -1,3 +1,3 @@
 cd ..
-.\build\windows\Win32Release\pic.exe -x 256 -y 5116 -t 24000 -l 40 --inject-width 176
+.\build\windows\Win32Release\pic.exe -x 256 -y 5116 -t 24000 -l 40 --inject-width 116
 cd launch_scripts
