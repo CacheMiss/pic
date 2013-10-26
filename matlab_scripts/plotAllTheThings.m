b89ea9e7d1000b9f1fd2789e472e22e42bdbadde
Stephen Sewell <stephen.sewell@gmail.com>
2013-10-26 13:04:59 -0500

    Tweaked plot everything script

--- matlab_scripts/plotAllTheThings.m
+++ matlab_scripts/plotAllTheThings.m
@@ -3,17 +3,24 @@ function plotAllTheThings(startIdx, endIdx, stepSize)
         idxStr = sprintf('%06d', i);
         eleName = strcat('ele_', idxStr);
         ionName = strcat('ion_', idxStr);
-        %rhoName = strcat('rho_', idxStr);
+        rhoName = strcat('rho_', idxStr);
+        rhoeName = strcat('rhoe_', idxStr);
+        rhoiName = strcat('rhoi_', idxStr);
         phiName = strcat('phi_', idxStr);
         phiAvgName = strcat('phiAvg_', idxStr);
         
         plotPart(eleName, 128);
         plotPart(ionName, 128);
         plotRho(idxStr, 128);
+        contourPhi(rhoName);
+        %plotPhiAll(rhoiName, 4, 32);
+        contourRho(rhoiName, 4, 32);
+        %contourRhoBottom(rhoiName);
         plotPhi(phiName, 128);
         plotPhi(phiAvgName, 128);
         plotPhiAll(phiName, 4, 32);
         contourPhi(phiName);
+        contourPhi(phiAvgName);
         
         % Find all windows of type figure, which have an empty FileName attribute.
         allPlots = findall(0, 'Type', 'figure', 'FileName', []);
