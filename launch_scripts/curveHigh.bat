5c89fe2afbbe06cd4b98318d6a007e30f72bee21
Stephen Sewell <stephen.sewell@gmail.com>
2013-11-02 13:52:30 -0500

    CurveHigh script now has a saner b field setting on Windows

--- launch_scripts/curveHigh.bat
+++ launch_scripts/curveHigh.bat
@@ -1,1 +1,1 @@
-..\build\windows\x64Release\pic.exe -x 256 -y 5116 -t 300000 -l 200 --inject-width 64 -o ..\run_output
+..\build\windows\x64Release\pic.exe -x 256 -y 5116 -t 300000 -l 200 --inject-width 64 -o ..\run_output --b0 3
