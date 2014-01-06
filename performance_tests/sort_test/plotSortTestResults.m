8d7c74a1540300cea4b0a9d6797ca9bc10c1f266
Stephen Sewell <stephen.sewell@gmail.com>
2014-01-06 08:19:26 -0600

    Changed . lines to - lines when printing sort benchmark results

--- performance_tests/sort_test/plotSortTestResults.m
+++ performance_tests/sort_test/plotSortTestResults.m
@@ -13,7 +13,7 @@ function plotSortTestResults
     f = figure;
     plot(x(:, 1), x(:, 5), '-', ...
          x(:,1), x(:,4), '-.', ...
-         x(:,1), x(:,7), ':');
+         x(:,1), x(:,7), '--');
     legend('GPU Sort', 'CPU Sort(with copy)', 'CPU Sort(without copy)');
     xlabel('Number of Particles');
     ylabel('Time to Sort (ms)');
@@ -22,7 +22,7 @@ function plotSortTestResults
     f = figure;
     plot(x(:, 1), x(:, 6), '-', ...
          x(:,1), x(:,7), '-.', ...
-         x(:,1), x(:,8), ':');
+         x(:,1), x(:,8), '--');
     legend('Copy To Host', 'CPU Sort', 'Copy To Device');
     xlabel('Number of Particles');
     ylabel('Time to Sort (ms)');
