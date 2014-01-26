ea21c7d079d3d40d63e1b8b7ace36cda909803f6
Stephen Sewell <stephen.sewell@gmail.com>
2014-01-25 18:56:34 -0600

    Added the capability to plot only hot or only cold particles

--- matlab_scripts/loadParticles.m
+++ matlab_scripts/loadParticles.m
@@ -15,63 +15,85 @@ function [hot cold] = loadParticles(fileName, varargin)
    fseek(f, 4, 'cof'); % Skip numParticles
    numHot = fread(f, 1, 'int32');
    numCold = fread(f, 1, 'int32');
-   fprintf('Num hot = %d\nNum cold = %d\n', numHot, numCold);
+   if optArgs.loadHot
+       fprintf('Num hot = %d\n', numHot);
+   end
+   if optArgs.loadCold
+       fprintf('Num cold = %d\n', numCold);
+   end
    sizeOfFloat = 4;
    
-   maxHot = optArgs.maxParticles;
-   maxCold = optArgs.maxParticles;
+   maxHot = numHot;
+   maxCold = numCold;
    sliceHot = 1;
    sliceCold = 1;
    
-   if(numHot > optArgs.maxParticles)
-       sliceHot = floor(numHot / maxHot);
-       fprintf('Limiting number of hot particles to %d\n', ...
-           optArgs.maxParticles);
-   end
-   if(numCold > optArgs.maxParticles)
-       sliceCold = floor(numCold / maxCold);
-       fprintf('Limiting number of cold particles to %d\n', ...
-           optArgs.maxParticles);
+   if optArgs.enableCulling
+       if(optArgs.loadHot && numHot > optArgs.maxParticles)
+           maxHot = optArgs.maxParticles;
+           sliceHot = floor(numHot / maxHot);
+           fprintf('Limiting number of hot particles to %d\n', ...
+               optArgs.maxParticles);
+       end
+       if(optArgs.loadCold && numCold > optArgs.maxParticles)
+           maxCold = optArgs.maxParticles;
+           sliceCold = floor(numCold / maxCold);
+           fprintf('Limiting number of cold particles to %d\n', ...
+               optArgs.maxParticles);
+       end
    end
    
-   hot = particleNull(maxHot);
-   cold = particleNull(maxCold);
+   if optArgs.loadHot
+       hot = particleNull(maxHot);
+   else
+       hot = particleNull(0);
+   end
+   
+   if optArgs.loadCold
+       cold = particleNull(maxCold);
+   else
+       cold = particleNull(0);
+   end
 
    % Store the location of the beginning of the hot particles
    dataStart = ftell(f);
    
    floatsPerPart = 5;
    
-   % Read hot particles
-   skipBytes = sizeOfFloat * ...
-       (floatsPerPart - 1 + floatsPerPart * sliceHot);
-   hot.x = fread(f, numHot, 'float', skipBytes);
-   advanceToField(f, dataStart, 2);
-   hot.y = fread(f, numHot, 'float', skipBytes);
-   advanceToField(f, dataStart, 3);
-   hot.vx = fread(f, numHot, 'float', skipBytes);
-   advanceToField(f, dataStart, 4);
-   hot.vy = fread(f, numHot, 'float', skipBytes);
-   advanceToField(f, dataStart, 5);
-   hot.vz = fread(f, numHot, 'float', skipBytes);
-   
-   % Reposition file pointer to the beginning of the cold particles
-   fseek(f, dataStart, 'bof');
-   fseek(f, sizeOfFloat * 5 * numHot, 'cof');
-   % Store the location of the beginning of the cold particles
-   dataStart = ftell(f);
-   
-   skipBytes = sizeOfFloat * ...
-       (floatsPerPart - 1 + floatsPerPart * sliceCold);
-   cold.x = fread(f, numCold, 'float', skipBytes);
-   advanceToField(f, dataStart, 2);
-   cold.y = fread(f, numCold, 'float', skipBytes);
-   advanceToField(f, dataStart, 3);
-   cold.vx = fread(f, numCold, 'float', skipBytes);
-   advanceToField(f, dataStart, 4);
-   cold.vy = fread(f, numCold, 'float', skipBytes);
-   advanceToField(f, dataStart, 5);
-   cold.vz = fread(f, numCold, 'float', skipBytes);
+   if optArgs.loadHot
+       % Read hot particles
+       skipBytes = sizeOfFloat * ...
+           (floatsPerPart - 1 + floatsPerPart * sliceHot);
+       hot.x = fread(f, maxHot, 'float', skipBytes);
+       advanceToField(f, dataStart, 2);
+       hot.y = fread(f, maxHot, 'float', skipBytes);
+       advanceToField(f, dataStart, 3);
+       hot.vx = fread(f, maxHot, 'float', skipBytes);
+       advanceToField(f, dataStart, 4);
+       hot.vy = fread(f, maxHot, 'float', skipBytes);
+       advanceToField(f, dataStart, 5);
+       hot.vz = fread(f, maxHot, 'float', skipBytes);
+   end
+
+   if optArgs.loadCold
+       % Reposition file pointer to the beginning of the cold particles
+       fseek(f, dataStart, 'bof');
+       fseek(f, sizeOfFloat * 5 * numHot, 'cof');
+       % Store the location of the beginning of the cold particles
+       dataStart = ftell(f);
+
+       skipBytes = sizeOfFloat * ...
+           (floatsPerPart - 1 + floatsPerPart * sliceCold);
+       cold.x = fread(f, maxCold, 'float', skipBytes);
+       advanceToField(f, dataStart, 2);
+       cold.y = fread(f, maxCold, 'float', skipBytes);
+       advanceToField(f, dataStart, 3);
+       cold.vx = fread(f, maxCold, 'float', skipBytes);
+       advanceToField(f, dataStart, 4);
+       cold.vy = fread(f, maxCold, 'float', skipBytes);
+       advanceToField(f, dataStart, 5);
+       cold.vz = fread(f, maxCold, 'float', skipBytes);
+   end
    
    fclose(f);
 end
@@ -82,17 +104,32 @@ function advanceToField(f, start, fieldNum)
    fseek(f, (fieldNum-1) * sizeOfFloat, 'cof');
 end
 
-function ret = parseArgs(varargin)
+function ret = parseArgs(args)
     ret = struct( ...
-        'maxParticles', 8000 ...
+        'enableCulling', true, ...
+        'maxParticles', 64000, ...
+        'loadHot', true, ...
+        'loadCold', true ...
         );
-    if ~isempty(varargin)
-        for i = 1:length(varargin)
-            if strcmp(varargin{i}(1), 'maxParticles')
-                ret.maxParticles = varargin{i}{2};
+    if ~isempty(args)
+        i = 1;
+        while i <= length(args)
+            if strcmp(args{i}, 'enableCulling')
+                ret.enableCulling = args{i+1};
+                i = i + 1;
+            elseif strcmp(args{i}, 'maxParticles')
+                ret.maxParticles = args{i+1};
+                i = i + 1;
+            elseif strcmp(args{i}, 'loadHot')
+                ret.loadHot = args{i+1};
+                i = i + 1;
+            elseif strcmp(args{i}, 'loadCold')
+                ret.loadCold = args{i+1};
+                i = i + 1;
             else
                 error('Invalid option!');
             end
+            i = i + 1;
         end
     end
 end
--- matlab_scripts/plotPart.m
+++ matlab_scripts/plotPart.m
@@ -1,4 +1,11 @@
-function ret = plotPart(fName)
+function ret = plotPart(fName, varargin)
+
+   if isempty(varargin)
+       % I still need initial values, even if I don't have arguments
+       optArgs = parseArgs; 
+   else
+       optArgs = parseArgs(varargin);
+   end
    
    fNameTokens = strsplit(fName, '_');
    
@@ -7,18 +14,36 @@ function ret = plotPart(fName)
        massRatio = 1;
    end
    
-   [hotP coldP] = loadParticles(fName);
+   [hotP coldP] = loadParticles(fName, ...
+       'maxParticles', optArgs.maxParticles, ...
+       'enableCulling', optArgs.enableCulling, ...
+       'loadHot', optArgs.plotHot, ...
+       'loadCold', optArgs.plotCold ...
+       );
    hotP = particleMix(hotP);
    coldP = particleMix(coldP);
    energyHot = massRatio * (hotP.vx.^2 + hotP.vy.^2 + hotP.vz.^2) / 2;
    energyCold = massRatio * (coldP.vx.^2 + coldP.vy.^2 + coldP.vz.^2) / 2;
    
    dotScale = 20;
-   if ~ isempty(hotP)
+   stdDevMultiplier = 6;
+   
+   if ~ isempty(hotP) && optArgs.plotHot
+       stdHotVx = std(hotP.vx);
+       meanHotVx = mean(hotP.vx);
+       vxMaxHot = meanHotVx + stdHotVx * stdDevMultiplier;
+       vxMinHot = meanHotVx - stdHotVx * stdDevMultiplier;
+
+       stdHotVy = std(hotP.vy);
+       meanHotVy = mean(hotP.vy);
+       vyMaxHot = meanHotVy + stdHotVy * stdDevMultiplier;
+       vyMinHot = meanHotVy - stdHotVy * stdDevMultiplier;
+   
        xMax = 2^nextpow2(max(hotP.x));
-       % yMax = 2^nextpow2(max(hotP(2,:)));
+       % yMax = 2^nextpow2(max(hotP.vx));
        yMax = max(hotP.y);
        
+       % Plot Hot Particle Positions
        figure;
        scatter(hotP.x, hotP.y, energyHot/norm(energyHot)*dotScale, energyHot);
        colorbar;
@@ -29,13 +54,60 @@ function ret = plotPart(fName)
        axis([0 xMax 0 yMax]);
        outName = strcat(fNameTokens{1}, '_hot_', fNameTokens{2});
        print('-dpng', outName);
+       
+       % Plot Hot Particle Vx vs. Y
+       figure;
+       scatter(hotP.vx,hotP.y, 0.4)
+       fields = strsplit(fName, '_');
+       title(strcat([fields{1} ' hot ' fields{2} ' Vx']));
+       xlabel('vx');
+       ylabel('y');
+       axis([vxMinHot vxMaxHot 0 max(hotP.y)]);
+       fields = strsplit(fName, '_');
+       outFile = strcat(fields{1}, '_hot_vx_vs_y_', fields{2});
+       print('-dpng', outFile);
+
+       % Plot Hot Particle Vy vs. Y
+       figure;
+       scatter(hotP.vy,hotP.y, 0.4)
+       fields = strsplit(fName, '_');
+       title(strcat([fields{1} ' hot ' fields{2} ' Vy']));
+       xlabel('vy');
+       ylabel('y');
+       axis([vyMinHot vyMaxHot 0 max(hotP.y)]);
+       fields = strsplit(fName, '_');
+       outFile = strcat(fields{1}, '_hot_vy_vs_y_', fields{2});
+       print('-dpng', outFile);
+
+       % Plot Hot Particle Vy vs. Vx
+       figure;
+       scatter(hotP.vx, hotP.vy, 0.4)
+       fields = strsplit(fName, '_');
+       title(strcat([fields{1} ' hot ' fields{2} ' Vy vs Vx']));
+       xlabel('vx');
+       ylabel('vy');
+       axis([vxMinHot vxMaxHot vyMinHot vyMaxHot]);
+       fields = strsplit(fName, '_');
+       outFile = strcat(fields{1}, '_hot_vy_vs_vx_', fields{2});
+       print('-dpng', outFile);
    end
    
-   if ~ isempty(coldP)
-       xMax = max(xMax, 2^nextpow2(max(coldP.x)));
-       % yMax = max(yMax, 2^nextpow2(max(coldP(2,:))));
+   if ~ isempty(coldP) && optArgs.plotCold
+       stdColdVx = std(coldP.vx);
+       meanColdVx = mean(coldP.vx);
+       vxMaxCold = meanColdVx + stdColdVx * stdDevMultiplier;
+       vxMinCold = meanColdVx - stdColdVx * stdDevMultiplier;
+
+       stdColdVy = std(coldP.vy);
+       meanColdVy = mean(coldP.vy);
+       vyMaxCold = meanColdVy + stdColdVy * stdDevMultiplier;
+       vyMinCold = meanColdVy - stdColdVy * stdDevMultiplier;
+
+       xMax = 2^nextpow2(max(coldP.x));
+       % yMax = max(yMax, 2^nextpow2(max(hotP.vx)));
        yMax = max(coldP.y);
        
+       % Plot Cold Particle Positions
        figure;
        scatter(coldP.x, coldP.y, energyCold/norm(energyCold)*dotScale, energyCold);
        colorbar;
@@ -46,6 +118,71 @@ function ret = plotPart(fName)
        axis([0 xMax 0 yMax]);
        outName = strcat(fNameTokens{1}, '_cold_', fNameTokens{2});
        print('-dpng', outName);
+       
+       % Plot Cold Particle Vx vs. Y
+       figure;
+       scatter(coldP.vx, coldP.y, 0.4)
+       fields = strsplit(fName, '_');
+       title(strcat([fields{1} ' cold ' fields{2} ' Vx']));
+       xlabel('vx');
+       ylabel('y');
+       axis([vxMinCold vxMaxCold 0 max(coldP.y)]);
+       fields = strsplit(fName, '_');
+       outFile = strcat(fields{1}, '_cold_vx_vs_y_', fields{2});
+       print('-dpng', outFile);
+
+       % Plot Cold Particle Vy vs. Y
+       figure;
+       scatter(coldP.vy, coldP.y, 0.4)
+       fields = strsplit(fName, '_');
+       title(strcat([fields{1} ' cold ' fields{2} ' Vy']));
+       xlabel('vy');
+       ylabel('y');
+       axis([vyMinCold vyMaxCold 0 max(coldP.y)]);
+       fields = strsplit(fName, '_');
+       outFile = strcat(fields{1}, '_cold_vy_vs_y_', fields{2});
+       print('-dpng', outFile);
+
+       % Plot Cold Particle Vy vs. Vx
+       figure;
+       scatter(coldP.vx, coldP.vy, 0.4)
+       fields = strsplit(fName, '_');
+       title(strcat([fields{1} ' cold ' fields{2} ' Vy vs Vx']));
+       xlabel('vx');
+       ylabel('vy');
+       axis([vxMinCold vxMaxCold vyMinCold vyMaxCold]);
+       fields = strsplit(fName, '_');
+       outFile = strcat(fields{1}, '_cold_vy_vs_vx_', fields{2});
+       print('-dpng', outFile);
    end
 
+end
+
+function ret = parseArgs(args)
+    ret = struct( ...
+        'enableCulling', true, ...
+        'maxParticles', 64000, ...
+        'plotHot', true, ...
+        'plotCold', true ...
+        );
+    if ~isempty(args)
+        i = 1;
+        while i <= length(args)
+            if strcmp(args{i}, 'noCull')
+                ret.enableCulling = false;
+            elseif strcmp(args{i}, 'maxParticles')
+                ret.maxParticles = args{i+1};
+                i = i + 1;
+            elseif strcmp(args{i}, 'hotOnly')
+                ret.plotHot = true;
+                ret.plotCold = false;
+            elseif strcmp(args{i}, 'coldOnly')
+                ret.plotHot = false;
+                ret.plotCold = true;
+            else
+                error('Invalid option!');
+            end
+            i = i + 1;
+        end
+    end
 end
