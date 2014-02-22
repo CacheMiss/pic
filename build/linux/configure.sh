d9edde965e558304e928d8fc66f685b48933e423
Stephen Sewell <stephen.sewell@gmail.com>
2014-02-22 14:06:58 -0600

    Fixed a bug on the linux config script where the boost directory was not being properly assigned

--- build/linux/configure.sh
+++ build/linux/configure.sh
@@ -2,6 +2,6 @@
 
 CUDADIR=$(dirname $(dirname $(which nvcc)))
 sed -e "s|^CUDADIR=$|&${CUDADIR}|" \
-    -e "s|^BOOSTDIR=$|&${HOME}|" \
+    -e "s|^BOOSTDIR=$|&${HOME}/boost|" \
     -e "s|^TBB=$|&${HOME}/tbb|" \
     Makefile.template > Makefile
