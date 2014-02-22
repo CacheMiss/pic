176755d3a301d9edc4fb56c8a8e342df76a68cf9
Stephen Sewell <stephen.sewell@gmail.com>
2014-02-22 10:51:36 -0600

    The makefile config script now properly sets up the thread building block home

--- build/linux/configure.sh
+++ build/linux/configure.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 CUDADIR=$(dirname $(dirname $(which nvcc)))
-sed -e "s|^CUDADIR=$|&${CUDADIR}|" -e "s|^BOOSTDIR=$|&${HOME}|" Makefile.template > Makefile
+sed -e "s|^CUDADIR=$|&${CUDADIR}|" \
+    -e "s|^BOOSTDIR=$|&${HOME}|" \
+    -e "s|^TBB=$|&${HOME}/tbb|" \
+    Makefile.template > Makefile
